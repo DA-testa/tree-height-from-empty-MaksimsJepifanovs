b177cc4c96d85530e4df30bee80a079ce5bd06a1
MaksimsJepifanovs <125698977+MaksimsJepifanovs@users.noreply.github.com>
2023-03-04 12:34:23 +0000

    Uzd2 versija1 Maksims Jepifanovs 221RDB137

--- tree_height.py
+++ tree_height.py
@@ -2,25 +2,75 @@
 
 import sys
 import threading
-import numpy
+import numpy as np
 
 
 def compute_height(n, parents):
     # Write this function
     max_height = 0
     # Your code here
+
+    #root = np.where(parents == -1)[0]
+
+    prn1 = {}
+    prn2 = {}
+    for i in range(n) :
+        
+        if parents[i] == -1 : 
+            prn1[parents[i]] = -1
+        else :
+            prn1[parents[i]] = parents[parents[i]]
+        #print(parents[i])
+        pass
+    max_height += 1
+    
+    while True :
+        if len(prn1) > 0 :
+            for i in prn1 :
+                prn2[prn1[i]] = prn1[prn1[i]]
+            prn1.clear()
+        else :
+            for i in prn2 :
+                prn1[prn2[i]] = prn2[prn2[i]]
+            prn2.clear()
+
+        max_height += 1
+        if len(prn1) == 1 or len(prn2) == 1 : break
+
+    print(max_height)
     return max_height
 
 
 def main():
+    nodesNumber = 0
+    par = ""
     # implement input form keyboard and from files
+    
+    mode = input()
+    if mode[0] == "F" :
+        fname = "./test/" + input()
+        #print(fname)
+        f = open(fname, "r")
+        nodesNumber = int(f.readline())
+        par = f.readline()
+        f.close()
+
+    if mode == "I" :
+        nodesNumber = int(input())
+        par = input()
     
     # let user input file name to use, don't allow file names with letter a
     # account for github input inprecision
     
     # input number of elements
+    
     # input values in one variable, separate with space, split these values in an array
+    
+    parList = list(map(int, par.split()))
+    parArr = np.array(parList)
+    #for i in parents
     # call the function and output it's result
+    compute_height(nodesNumber, parArr)
     pass
 
 # In Python, the default limit on recursion depth is rather low,
@@ -29,5 +79,5 @@ def main():
 sys.setrecursionlimit(10**7)  # max depth of recursion
 threading.stack_size(2**27)   # new thread will get stack of such size
 threading.Thread(target=main).start()
-main()
+#main()
 # print(numpy.array([1,2,3]))
